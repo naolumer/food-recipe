308b5a850bfb159dfb705e11c6278be1cbdc8930
naolumer <naolumer3@gmail.com>
2025-03-10 15:08:48 +0300

    catch error if api is not available/working

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -17,12 +17,15 @@ const FoodList = ({food}) => {
     },[food])
 
   return (
+    
     <div className='mx-auto flex flex-col md:flex-row gap-2 w-[90vw] mt-10 rounded-md shadow-sm shadow-gray-700 flex-wrap items-center bg-slate-50 mb-16'>
-        {
+        {foodList? `${
             foodList.map((item)=>(
                 <FoodItem key={item.title} item={item}/>
             ))
+        }`: <h1 className='text-center text-3xl font-bold font-mono p-8 m-auto'>OOPS! API NOT WORKING AT THE MOMENT ⚠️</h1>
         }
+        
     </div>
   )
 }
--- src/pages/FoodRecipe.jsx
+++ src/pages/FoodRecipe.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const FoodRecipe = () => {
+
+  const [foodDetail,setFoodDetail] = useState({})
+  const [loading,setLoading] = useState(true)
+  const {id} = useParams()
+  const URL = `https://api.spoonacular.com/recipes/${id}/information`;
+  const API_KEY = "9c60d1ff8b3742f1bda437647aab4b29";
+
+  useEffect (()=>{
+    async function fetchDetails(){
+      const response = await fetch(`${URL}?apiKey=${API_KEY}`)
+      const data = await response.json()
+      setFoodDetail(data)
+    }
+    fetchDetails()
+    setLoading(false)
+
+  },[id])
+
   return (
     <div className='w-full mx-4 mt-8'>
+
+      <h1 className='text-3xl font-bold text-center'>Here is your recipe, Enjoy 🥣</h1>
+      {
+        loading ? "Loading...":
+        <div className='flex flex-col'>
+          <img src={foodDetail.image} alt="" />
+          <p>{foodDetail.title}</p>
+          <div className='flex items-center justify-around'>
+            <p>⌚ Cooking time : {foodDetail.readyInMinutes} </p>
+            <p>👪 Can serve : {foodDetail.servings} People</p>
+          </div>
+          <div className='flex items-center justify-around'>
+            <p>{foodDetail.vegetarian?"🥦 Vegetarian":"🍖 Non-vegetarian"}</p>
+            <p>{foodDetail.vegan ?"🐮 Vegan":"🍊 Animal Free"}</p>
+          </div>
+        </div>
+      }
       
+
 
     </div>
   )
